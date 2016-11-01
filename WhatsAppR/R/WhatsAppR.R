b498ad315b81b6c0cd16e664c1f97ca71cd06067
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-11-01 11:37:44 +0000

    staged updates properly

---
+++ WhatsAppR/R/WhatsAppR.R
@@ -0,0 +1,31 @@
+##' Visualising WhatsApp conversation data
+##' 
+##' @author Finlay Campbell <f.campbell15@@imperial.ac.uk>
+##' 
+##' @export
+##' 
+##' @param file the location of the WhatsApp txt file (including \\ replacement)
+##' @param save a logcal indicating if the output plots should be saved to the current directory
+##' @param OS indicating if the operating system is "android" or "iOS"
+##' @param wordcloud a logical indicating if a wordcloud should be plotted or not 
+
+WhatsAppR <- function(file,save=FALSE,OS="android",wordcloud=TRUE){
+  
+  input <- readLines(file)
+
+  data <- wr.data(input,OS)
+    
+  delays <- wr.delays(data)
+
+  plots <- wr.plot(data,delays,save)
+  
+  if(wordlcoud) wr.wordcloud(data)
+
+  out <- list()
+  out$data <- list(data=data,delays=delays)
+  out$plots <- plots
+
+  return(out)
+}
+
+#for(i in c("K","M","C","CC","AIDS","TC")) assign(paste("out",i,sep=""),WhatsAppR(paste("chat",i,sep=""),save=TRUE))
---
+++ WhatsAppR/R/wr.data.R
@@ -0,0 +1,40 @@
+wr.data <- function(input,OS){
+  
+  data <- data.frame(date=as.Date(substr(input,1,10),"%d/%m/%Y"))
+  
+  if(OS=="android"){
+    find.index <- 2
+    start_name <- 21
+    data$time <- chron::times(paste(substr(input,13,17),":00",sep=""))
+  }
+  
+  if(OS=="iOS"){
+    find.index <- 4
+    start_name <- 24
+    data$time <- chron::times(substr(input,13,20))
+  }
+  
+  data$person <- sapply(input,function(line) substr(line,start_name,gregexpr(":", line)[[1]][find.index]-1))
+  
+  data$text <- sapply(input,function(line) substr(line,gregexpr(":", line)[[1]][find.index]+2,nchar(line)))
+  
+  data$words <- sapply(data$text,function(line) length(strsplit(line," ")[[1]]))
+  
+  data$fulltime <- strptime(paste(data$date,data$time),format='%Y-%m-%d %H:%M')
+  
+  data <- data[order(data$fulltime),]
+  
+  #Remove 'people' that are found less than ten times (error when you have pasted wa conversations in chat)
+  people <- names(sort(table(data$person)[table(data$person)>10],decreasing=TRUE))
+  
+  data$person <- factor(data$person,levels=people)
+  
+  errors <- !data$person %in% people
+  
+  if(sum(errors) > 0.1*nrow(data)) stop("More than 10% of messages can't be read, error in input format")
+  
+  data <- data[!errors,]
+  
+  return(data)
+  
+}
