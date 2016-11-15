cca466b713b32618e44e901c773c32b9e95b74d8
Finlay Campbell <f.campbell15@imperial.ac.uk>
2016-11-15 17:47:00 +0000

    "Updated documentation"

--- R/WhatsAppR.R
+++ R/WhatsAppR.R
@@ -1,26 +1,26 @@
 
 ##' Visualising WhatsApp conversation data
-##' 
+##'
 ##' @author Finlay Campbell <f.campbell15@@imperial.ac.uk>
-##' 
-##' @return Returns a list of output plots
+##'
+##' @return Returns a named list of output plots
 ##' @export
-##' 
+##'
 ##' @param file the directory of the WhatsApp .txt file
 ##' @param save a logcal indicating if the output plots should be saved to the current directory
 ##' @param OS indicating if the operating system is "android" or "iOS"
-##' @param wordcloud a logical indicating if a wordcloud should be plotted or not 
+##' @param wordcloud a logical indicating if a wordcloud should be plotted or not
 
 WhatsAppR <- function(file,save=FALSE,OS="android",wordcloud=TRUE){
-  
+
   input <- readLines(file)
 
   data <- wr.data(input,OS)
-    
+
   delays <- wr.delays(data)
 
   plots <- wr.plot(data,delays,save)
-  
+
   if(wordcloud) wr.wordcloud(data)
 
   out <- list()
